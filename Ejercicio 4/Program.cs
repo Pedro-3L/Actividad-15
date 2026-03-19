c5b4e272081ddb950f2b2d2207c30a178f6d6750
Pedro-3L <lando133@gmail.com>
2026-03-19 17:40:57 -0600

    Adicion ejercicio 4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int año;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese un año de nacimiento");
+				correcto = int.TryParse(Console.ReadLine(), out año);
+				if (año < 1900 || año>2026)
+				{
+					correcto = false;
+					Console.WriteLine("Año invalido, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (!correcto);
+			Console.WriteLine("Año valido");
+
+		}
+	}
+}
