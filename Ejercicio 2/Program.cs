ebfdef7916ceff79d293b0277257fb0c03de9ead
Pedro-3L <lando133@gmail.com>
2026-03-19 17:14:10 -0600

    Adicion ejercicio 2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double producto;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese el precio de un producto");
+				correcto = double.TryParse(Console.ReadLine(), out producto);
+				if (producto <=0)
+				{
+					correcto = false;
+					Console.WriteLine("Producto invalido, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (correcto == false);
+			Console.WriteLine("Producto Valido");
+		}
+	}
+}
