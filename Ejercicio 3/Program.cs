729054b724381ea3beb3e885adc5f95f5275c7ef
Pedro-3L <lando133@gmail.com>
2026-03-19 17:32:16 -0600

    Adicion ejercicio 3

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int cantidad;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese la cantidad de estudiantes del curso");
+				correcto = int.TryParse(Console.ReadLine(), out cantidad);
+				if (cantidad<0)
+				{
+					correcto = false;
+					Console.WriteLine("Cantidad invalida, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (!correcto);
+			Console.WriteLine("Cantidad valida");
+		}
+	}
+}
