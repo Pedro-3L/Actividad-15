f123e90021eb863187a03baf82e95c627348b2ef
Pedro-3L <lando133@gmail.com>
2026-03-19 16:22:39 -0600

    adicion ejercicio 1

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int edad;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese una edad");
+				correcto = int.TryParse(Console.ReadLine(), out edad);
+				if (edad <1  || edad > 100)
+				{
+					correcto = false;
+					Console.WriteLine("Edad Invalida, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (correcto==false);
+				Console.WriteLine("Edad Valida");
+
+
+		}
+	}
+}
