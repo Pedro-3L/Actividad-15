9b12830fc4862486c328ea00e94a75f9f95af16a
Pedro-3L <lando133@gmail.com>
2026-03-19 19:40:00 -0600

    Adicion ejercicio 10

---
+++ Ejercicio #10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio__10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int cantidad;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese la cantidad de productos que desee:");
+				correcto = int.TryParse(Console.ReadLine(), out cantidad);
+				if (cantidad <= 0)
+				{
+					correcto = false;
+					Console.WriteLine("Cantidad inválido, intentelo otra vez");
+					Console.ReadKey();
+				}
+			} while (correcto == false);
+
+			Console.WriteLine("Cantidad valida");
+		}
+	}
+}
