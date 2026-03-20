29193d3607495d57c75e72b3f958c30e6e95ab65
Pedro-3L <lando133@gmail.com>
2026-03-19 18:17:22 -0600

    Adicion Ejercicio 6

---
+++ Ejercicio 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime fecha;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese una fecha");
+				correcto = DateTime.TryParse(Console.ReadLine(), out fecha);
+				if (!correcto)
+				{
+					Console.WriteLine("Fecha invalida, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (!correcto);
+			Console.WriteLine("Fecha valida " + fecha);
+		}
+	}
+}
