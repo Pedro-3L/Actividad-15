2e0f98be303791b974fe5707baeb4b3523b84fcb
Pedro-3L <lando133@gmail.com>
2026-03-19 19:22:45 -0600

    Adicion ejercicio 9

---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int nota;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese la nota del estudiante");
+				correcto = int.TryParse(Console.ReadLine(), out nota);
+				if (nota < 0 || nota > 100)
+				{
+					correcto = false;
+					Console.WriteLine("Nota invalida, intentelo otra vez");
+					Console.ReadKey();
+				}
+			} while (correcto == false);
+			Console.WriteLine("Nota valida");
+			if (nota >= 61)
+			{
+				Console.WriteLine("Aprobo");
+			}
+			else
+			{
+				Console.WriteLine("Reprobo");
+			}
+
+
+		}
+	}
+}
