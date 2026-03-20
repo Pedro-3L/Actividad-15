af6815ad9be10c1dbe9d2c7b4c66c56d026df089
Pedro-3L <lando133@gmail.com>
2026-03-19 18:11:28 -0600

    adicion ejercicio 5

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero;
+bool correcto;
+do
+{
+	Console.Clear();
+	Console.WriteLine("Ingrese un numero");
+	correcto = int.TryParse(Console.ReadLine(), out numero);
+	if (!correcto)
+	{
+		Console.WriteLine("Numero invalido, intentelo otra vez");
+		Console.ReadKey();
+	}
+
+} while (!correcto);
+if (numero % 2 == 0)
+{
+	Console.WriteLine("Numero valido, es par");
+}
+else
+{
+	Console.WriteLine("Numero valido, es impar");
+}
