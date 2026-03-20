6d53a29d8025b3cf1bd5b713a3faea3326d2d5f3
Pedro-3L <lando133@gmail.com>
2026-03-19 18:35:08 -0600

    adicion de ejercicio 7 y 8

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double salario;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese el salario de un empelado ");
+				correcto = double.TryParse(Console.ReadLine(), out salario);
+				if (salario<0)
+				{
+					correcto = false;
+					Console.WriteLine("Salario invalido, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (!correcto);
+			Console.WriteLine("Salario valido");
+		}
+	}
+}
---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double resultado;
+			double numero;
+			bool correcto;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Ingrese un numero");
+				correcto = double.TryParse(Console.ReadLine(), out numero);
+				if (numero < 0)
+				{
+					correcto = false;
+					Console.WriteLine("Numero inválido, intentelo otra vez");
+					Console.ReadKey();
+				}
+
+			} while (correcto == false);
+
+			resultado = Math.Sqrt(numero);
+
+			Console.WriteLine("La raíz cuadrada es: " + resultado);
+		}
+	}
+}
